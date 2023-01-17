305fd92c57f20b283a74a38d5451728f59900a3d
weycy <wcy1015@126.com>
2023-01-17 12:40:30 +0800

    笔记本列表样式

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,25 +1,55 @@
 <template>
-<div id="notebook-list">
-  <h1>{{msg}}</h1>
-  <ul>
-    <li><router-link to="/note/1">笔记本1</router-link></li>
-    <li><router-link to="/note/2">笔记本2</router-link></li>
-  </ul>
-</div>
+  <div class="detail" id="notebook-list">
+    <header>
+      <a href="#" class="btn"><i class="iconfont icon-plus"></i>新建笔记本</a>
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记本列表(3)</h3>
+        <div class="book-list">
+          <router-link to="" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"/>笔记本标题1
+              <span>3</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">3天前</span>
+            </div>
+          </router-link>
+          <router-link to="" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"/>笔记本标题2
+              <span>1</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">5天前</span>
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import Auth from "../apis/auth";
+
 export default {
   data() {
     return {
-      msg:'笔记本列表'
+      msg: '笔记本列表'
     }
+  },
+  created() {
+    Auth.getInfo().then(res => {
+      if (!res.isLogin) {
+        this.$router.push({path: '/login'})
+      }
+    })
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
-}
+<style lang="less" scoped>
+ @import url(../assets/css/notebook-list.less);
 </style>
