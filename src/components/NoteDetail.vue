35481ae785234385edd1769068c0562ea609934f
weycy <wcy1015@126.com>
2023-01-20 20:45:27 +0800

    笔记详情页编辑区结构样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="note" class="detail">
     <note-sidebar></note-sidebar>
-    <h1></h1>
-    <h1></h1>
+    <div class="note-detail">
+      <div class="note-empty" v-show="false">请选择笔记</div>
+      <div class="note-detail-ct" v-show="true">
+        <div class="note-bar">
+          <span>创建日期：2天前</span>
+          <span>更新日期：1分钟前</span>
+          <span>已保存</span>
+          <span class="iconfont icon-delete"/>
+          <span class="iconfont icon-fullscreen"/>
+        </div>
+        <div class="note-title">
+          <input type="text" placeholder="输入标题">
+        </div>
+        <div class="editor">
+          <textarea v-show="true" placeholder="输入内容，支持 markdown"/>
+          <div class="preview markdown-body" v-show="false"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,7 +45,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+@import url(../assets/css/note-detail);
+
 #note {
   display: flex;
   align-items: stretch;
