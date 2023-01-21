72e3129337c3ee7d087d5f687adf776b9566fb38
weycy <wcy1015@126.com>
2023-01-22 02:37:03 +0800

    使用vue-codeMirror优化笔记编辑区

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -9,16 +9,19 @@
           <span>更新日期：{{ curNote.updatedAtFriendly }}</span>
           <span>{{ statusText }}</span>
           <span class="iconfont icon-delete" @click="onDeleteNote" title="删除笔记"/>
-          <span class="iconfont" :class="isShowPreview?'icon-edit':'icon-eye'"  @click="isShowPreview = !isShowPreview" :title="isShowPreview?'编辑模式':'预览模式'"/>
+          <span class="iconfont" :class="isShowPreview?'icon-edit':'icon-eye'" @click="isShowPreview = !isShowPreview"
+                :title="isShowPreview?'编辑模式':'预览模式'"/>
         </div>
         <div class="note-title">
           <input type="text" v-model:value="curNote.title" @input="onUpdateNote" @keydown="statusText='正在输入...'"
                  placeholder="输入标题">
         </div>
         <div class="editor">
-          <textarea v-show="isShowPreview" v-model:value="curNote.content" @input="onUpdateNote"
-                    @keydown="statusText='正在输入...'" placeholder="输入内容，支持 markdown"/>
-          <div class="preview markdown-body" v-html="previewContent" v-show="!isShowPreview"/>
+          <codemirror class="codemirror" v-model="curNote.content" :options="cmOptions" v-show="!isShowPreview"
+                      @input="onUpdateNote" @inputRead="statusText='正在输入...'"></codemirror>
+          <!--          <textarea v-show="isShowPreview" v-model:value="curNote.content" @input="onUpdateNote"-->
+          <!--                    @keydown="statusText='正在输入...'" placeholder="输入内容，支持 markdown"/>-->
+          <div class="preview markdown-body" v-html="previewContent" v-show="isShowPreview"/>
         </div>
       </div>
     </div>
@@ -32,17 +35,32 @@ import Bus from "../helpers/bus"
 import _ from 'lodash';
 import Notes from "../apis/notes";
 import MarkdownIt from 'markdown-it'
+import {codemirror} from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/markdown/markdown.js'
+import 'codemirror/theme/neat.css'
 import {mapState, mapGetters, mapMutations, mapActions} from "vuex";
 
 
 let md = new MarkdownIt()
 export default {
-  components: {noteSidebar},
+  components: {
+    noteSidebar,
+    codemirror
+  },
 
   data() {
     return {
       statusText: '笔记未改动',
-      isShowPreview: false
+      isShowPreview: false,
+      cmOptions: {
+        tabSize: 4,
+        mode: 'text/x-markdown',
+        theme: 'neat',
+        lineNumbers: false,
+        line: true,
+        // more codemirror options, 更多 codemirror 的高级配置...
+      }
     }
   },
   created() {
@@ -74,7 +92,7 @@ export default {
         }).catch(data => {
         this.statusText = '保存出错'
       })
-    }, 300),
+    }, 3000),
 
     onDeleteNote() {
       this.deleteNote({noteId: this.curNote.id})
@@ -107,4 +125,5 @@ export default {
   background: #fff;
   flex: 1;
 }
+
 </style>
