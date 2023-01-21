ae6c212b805d6f6b0325c5d39dc2b0096aa4246d
weycy <wcy1015@126.com>
2023-01-21 10:39:43 +0800

    引入vuex改造笔记本侧边栏组件

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import notebook from './modules/notebook'
+import note from './modules/note'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     notebook,
+    note
   }
 })
