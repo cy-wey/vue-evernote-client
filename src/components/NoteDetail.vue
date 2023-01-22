bdc6b9c3c6a84b474adb15612ac253f1855a226e
weycy <wcy1015@126.com>
2023-01-22 20:09:57 +0800

    封装 markdown-it

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -33,62 +33,13 @@
 <script>
 import noteSidebar from "./NoteSidebar";
 import _ from 'lodash';
-import MarkdownIt from 'markdown-it'
 import {codemirror} from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/markdown/markdown.js'
 import 'codemirror/theme/neat.css'
 import 'codemirror/addon/display/placeholder.js'
+import {md} from '../lib/markdown-it'
 import {mapState, mapGetters, mapMutations, mapActions} from "vuex";
-import hljs from "highlight.js/lib/core";
-import "highlight.js/styles/github-dark.css";
-
-import bash from 'highlight.js/lib/languages/bash'
-import javascript from 'highlight.js/lib/languages/javascript';
-import typescript from 'highlight.js/lib/languages/typescript';
-import java from 'highlight.js/lib/languages/java';
-import sql from 'highlight.js/lib/languages/sql';
-import nginx from 'highlight.js/lib/languages/nginx';
-import json from 'highlight.js/lib/languages/json';
-import yaml from 'highlight.js/lib/languages/yaml';
-import xml from 'highlight.js/lib/languages/xml';
-import shell from 'highlight.js/lib/languages/shell'
-
-hljs.registerLanguage('bash', bash)
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('typescript', typescript);
-hljs.registerLanguage('java', java);
-hljs.registerLanguage('sql', sql);
-hljs.registerLanguage('nginx', nginx);
-hljs.registerLanguage('json', json);
-hljs.registerLanguage('yaml', yaml);
-hljs.registerLanguage('xml', xml);
-hljs.registerLanguage('shell', shell);
-
-let md = new MarkdownIt({
-  html: true,
-  linkify: true,
-  breaks: true,
-  xhtmlOut: true,
-  typographer: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return (
-          '<pre class="hljs"><code>' +
-          hljs.highlight(lang, str, true).value +
-          "</code></pre>"
-        );
-      } catch (__) { }
-    }
-
-    return (
-      '<pre class="hljs"><code>' +
-      md.utils.escapeHtml(str) +
-      "</code></pre>"
-    );
-  },
-})
 
 export default {
   components: {
---
+++ src/lib/markdown-it.js
@@ -0,0 +1,53 @@
+import MarkdownIt from 'markdown-it'
+import hljs from "highlight.js/lib/core";
+import bash from 'highlight.js/lib/languages/bash'
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import java from 'highlight.js/lib/languages/java';
+import sql from 'highlight.js/lib/languages/sql';
+import nginx from 'highlight.js/lib/languages/nginx';
+import json from 'highlight.js/lib/languages/json';
+import yaml from 'highlight.js/lib/languages/yaml';
+import xml from 'highlight.js/lib/languages/xml';
+import shell from 'highlight.js/lib/languages/shell'
+
+import "highlight.js/styles/github-dark.css";  // 高亮样式
+import "./css/markdown-it.less"
+
+hljs.registerLanguage('bash', bash)
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('nginx', nginx);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('shell', shell);
+
+let md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  breaks: true,
+  xhtmlOut: true,
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return (
+          '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          "</code></pre>"
+        );
+      } catch (__) { }
+    }
+
+    return (
+      '<pre class="hljs"><code>' +
+      md.utils.escapeHtml(str) +
+      "</code></pre>"
+    );
+  },
+})
+
+export {md}
